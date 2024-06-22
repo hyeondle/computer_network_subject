b9aa88daf9a1478bc0bf6dfd90dfcfc5ad8752e6
Hyeondong Lee <hyeondle@cx1r1s3.42seoul.kr>
2024-06-22 12:33:17 +0900

    basic connection add

---
+++ server/client_handler.c
@@ -0,0 +1,15 @@
+#include "server.h"
+
+void *handle_clnt(void *arg) {
+    t_connected *client;
+    int sock;
+    int client_cnt;
+    int str_len = 0;
+    int order = 0;
+    char msg[BUF_SIZE];
+    char *text;
+
+    client = (t_connected *)arg;
+
+	return 0;
+}
---
+++ server/server.h
@@ -0,0 +1,64 @@
+#ifndef SERVER_H
+# define SERVER_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+
+# define BUF_SIZE 1024
+# define MAX_CLNT 256
+
+typedef struct s_map {
+    char *key;
+    int value;
+}   t_map;
+
+typedef struct s_server {
+    int serv_sock;
+    int clnt_sock;
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+
+    int clnt_cnt;
+    int clnt_socks[MAX_CLNT];
+    int clnt_addr_size;
+
+    t_map list[MAX_CLNT];
+}   t_server;
+
+typedef struct s_mutex_list {
+    pthread_mutex_t *s_a;
+}   t_mutex_list;
+
+typedef struct s_connected {
+    int clnt_sock;
+    int clnt_cnt;
+    char name[20];
+
+    t_server *server;
+    t_mutex_list *mutex_list;
+    pthread_t *t_id;
+}   t_connected;
+
+//
+// init.c
+//
+t_mutex_list *init_mutex();
+t_server *init_server(char **argv);
+
+//
+// server.c
+//
+void run_server(t_server *server, t_mutex_list *mutex_list);
+
+//
+// client_handler.c
+//
+void *handle_clnt(void *arg);
+
+
+#endif
